edcc4dc4aaed6a7094a77806f408f0a3bbcbfc27
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-08-17 12:23:32 +0300

    Avoid recursive default AI task-local context

--- Travis_Multiplatform_Ai/Services/AIExecutionScope.swift
+++ Travis_Multiplatform_Ai/Services/AIExecutionScope.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 enum AIExecutionScope {
-    @TaskLocal static var context: AIInvocationContext = .general
+    @TaskLocal static var context: AIInvocationContext = AIInvocationContext(
+        workload: .routine,
+        capabilityId: nil,
+        taskId: nil,
+        stepId: nil,
+        projectId: nil,
+        operation: nil
+    )
 }
